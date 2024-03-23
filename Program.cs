e1a978a6127489f328ac4d35b97b0a7290c045da
Black <Black@DESKTOP-COVM106>
2024-03-23 02:36:32 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MayaEngineX33
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
